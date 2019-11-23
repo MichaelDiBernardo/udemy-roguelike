d24fc3ee6b9499c1376afd32f1bc0f594a389747
MichaelDiBernardo <mikedebo@gmail.com>
2019-11-23 16:36:01 -0500

    After lecture 9.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,37 @@
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public Rigidbody2D physics;
+
+    public Transform gunArm;
 
     private Vector2 _moveInput;
 
-    // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        move();
+        aim();
+    }
+
+    private void move()
     {
         _moveInput.x = Input.GetAxisRaw("Horizontal");
         _moveInput.y = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(
-            _moveInput.x * Time.deltaTime * moveSpeed,
-            _moveInput.y * Time.deltaTime * moveSpeed,
-            0f
-        );
+        physics.velocity = _moveInput * moveSpeed;
+    }
+
+    private void aim()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector3 offset = mousePosition - screenPoint;
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
