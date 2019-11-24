8a582379ab12ce82ac15394ac320c57f71f7ac7c
MichaelDiBernardo <mikedebo@gmail.com>
2019-11-24 09:55:45 -0500

    End of lecture 10.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D physics;
-
     public Transform gunArm;
 
     private Vector2 _moveInput;
+    private Camera _camera;
 
     void Start()
     {
+        _camera = Camera.main;
     }
 
     void Update()
@@ -30,8 +31,19 @@ public class PlayerController : MonoBehaviour
     private void aim()
     {
         Vector3 mousePosition = Input.mousePosition;
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector3 screenPoint = _camera.WorldToScreenPoint(transform.localPosition);
         Vector3 offset = mousePosition - screenPoint;
+
+        if (Mathf.Sign(offset.x) <= 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
 
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunArm.rotation = Quaternion.Euler(0f, 0f, angle);
