a8a0538bc5a9c2c30bee5bfa77ec23db2e113002
MichaelDiBernardo <mikedebo@gmail.com>
2019-11-25 21:45:26 -0500

    End of lecture 13.

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 7.5f;
+    public Rigidbody2D physics;
+
+    void Start()
+    {
+        // Video directed us to put this in Update() -- why?
+        physics.velocity = transform.right * speed;
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+    }
+}
