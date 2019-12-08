8508c0764d90bccf4c89d788ac1f2b213d486b23
MichaelDiBernardo <mikedebo@gmail.com>
2019-12-08 12:43:46 -0500

    End of lecture 31.

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        doTrapEffect(other);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        doTrapEffect(other);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        doTrapEffect(other.collider);
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        doTrapEffect(other.collider);
+    }
+
+    private static void doTrapEffect(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealthController.instance.DamagePlayer();
+        }
+    }
+}
