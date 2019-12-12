cfc4d3bb9c174201ea3b10977079b8c3308b8018
MichaelDiBernardo <mikedebo@gmail.com>
2019-12-11 21:41:18 -0500

    End of lecture 35.

---
+++ Assets/Scripts/Breakables.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Breakables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && PlayerController.instance.IsDashing)
+        {
+            Destroy(gameObject);               
+        }
+    }
+}
