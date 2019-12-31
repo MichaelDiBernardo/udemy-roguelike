3f972d6ad95c0738f4d3870f0c0df976d515478f
MichaelDiBernardo <mikedebo@gmail.com>
2019-12-31 12:07:47 -0500

    End of lecture 39.

---
+++ Assets/Scripts/SpriteSortOrder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrder : MonoBehaviour
+{
+    private SpriteRenderer _theSR;
+
+    private void Awake()
+    {
+        _theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _theSR.sortingOrder = Mathf.RoundToInt(-100 * transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
