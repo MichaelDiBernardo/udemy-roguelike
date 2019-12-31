06750749ed270075db187d336dfa53a95afe2d06
MichaelDiBernardo <mikedebo@gmail.com>
2019-12-31 12:51:09 -0500

    End of lecture 41.

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Breakables : MonoBehaviour
 {
     public GameObject[] brokenPieces;
     public int maxShrapnel;
+
+    public GameObject[] drops;
+    public float dropPercent;
 
     // Start is called before the first frame update
     void Start()
@@ -22,15 +27,36 @@ public class Breakables : MonoBehaviour
         if (other.CompareTag("Player") && PlayerController.instance.IsDashing)
         {
             Destroy(gameObject);
-
-            int piecesToDrop = Random.Range(1, maxShrapnel + 1);
-
-            for (int i=0; i<piecesToDrop; i++)
-            {
-                int randomPiece = Random.Range(0, brokenPieces.Length);
-                Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
-            }
-            
+            BlowUp();
+            MaybeDropItem();
         }
+    }
+
+    private void BlowUp()
+    {
+        int piecesToDrop = Random.Range(1, maxShrapnel + 1);
+
+        for (int i = 0; i < piecesToDrop; i++)
+        {
+            int randomPiece = Random.Range(0, brokenPieces.Length);
+            Instantiate(brokenPieces[randomPiece], transform.position, transform.rotation);
+        }
+    }
+
+
+    private void MaybeDropItem()
+    {
+        if (drops.Length == 0)
+        {
+            return;
+        }
+
+        if (Random.Range(0f, 100f) >= dropPercent)
+        {
+            return;
+        }
+
+        int dropIndex = Random.Range(0, drops.Length);
+        Instantiate(drops[dropIndex], transform.position, transform.rotation);
     }
 }
--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -3,22 +3,31 @@
 public class HealthPickup : MonoBehaviour
 {
     public int healAmount = 1;
+    public float timeBeforeCollectable = 0.5f;
+
+    private FrameTimer _collectTimer;
+    private bool _isCollectable = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _collectTimer = new FrameTimer(timeBeforeCollectable, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_isCollectable)
+        {
+            return;
+        }
+
+        _isCollectable = _collectTimer.CheckThisFrame();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && _isCollectable)
         {
             PlayerHealthController.instance.HealPlayer(healAmount);
             Destroy(gameObject);
