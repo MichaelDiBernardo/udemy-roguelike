487f7fdc9e1bf405031937319599b21e4bb4b36c
MichaelDiBernardo <mikedebo@gmail.com>
2020-01-05 16:58:53 -0500

    End of lecture 52.

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,8 +5,35 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     public bool doorsCloseOnEnter;
+    public bool openWhenEnemiesClear;
 
     public GameObject[] doors;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private bool _playerInRoom;
+
+    void Update()
+    {
+        if (!_playerInRoom || !openWhenEnemiesClear)
+        {
+            return;
+        }
+                
+        for (int i=0; i < enemies.Count; i++)
+        {
+            if (enemies[i] == null)
+            {
+                enemies.RemoveAt(i);
+                i--;
+            }
+        }
+
+        if (enemies.Count == 0)
+        {
+            RemoveDoors();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,8 +42,17 @@ public class Room : MonoBehaviour
             return;
         }
 
+        _playerInRoom = true;
         MoveCameraToHere();
         MaybeActivateDoors();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInRoom = false;
+        }
     }
 
     private void MaybeActivateDoors()
@@ -35,5 +71,14 @@ public class Room : MonoBehaviour
     private void MoveCameraToHere()
     {
         CameraController.instance.SetTarget(transform.position);
+    }
+
+    private void RemoveDoors()
+    {
+        foreach (var door in doors)
+        {
+            door.SetActive(false);
+        }
+        doorsCloseOnEnter = false;
     }
 }
