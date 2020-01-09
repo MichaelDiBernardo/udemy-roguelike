74e6a04a0e8ef9156b6513321a910d96a47e1efc
MichaelDiBernardo <mikedebo@gmail.com>
2020-01-08 19:54:48 -0500

    End of lecture 55.

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -3,26 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    public string nextLevel;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player"))
         {
             return;
-        }        
-        SceneManager.LoadScene(nextLevel);
+        }
+
+        LevelManager.instance.EndLevel();
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
+
+    public float delayBetweenLevels = 4f;
+
+    public string nextLevel; 
 
     public Room CurrentRoom { get; set; }
 
     private void Awake()
     {
         instance = this;
+    }
+
+    public void EndLevel()
+    {
+        StartCoroutine(SwitchLevels(nextLevel));
+    }
+
+    private IEnumerator SwitchLevels(string levelName)
+    {
+        AudioManager.instance.PlayWinLevel();
+        yield return new WaitForSeconds(delayBetweenLevels);
+        SceneManager.LoadScene(levelName);
     }
 }
