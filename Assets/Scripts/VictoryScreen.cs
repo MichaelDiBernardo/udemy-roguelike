4f12728dac47aaf70ef8f60b56277aa908ee1243
MichaelDiBernardo <mikedebo@gmail.com>
2020-01-11 16:24:57 -0500

    After lecture 59.

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScreen : MonoBehaviour
+{
+    public float TimeToWaitForInstructions = 2f;
+    public GameObject Instructions;
+    public string MainMenuScene;
+
+    private bool _canExit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayInstructions());        
+    }
+
+    void Update()
+    {
+        if (_canExit && Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(MainMenuScene);
+        }
+    }
+
+    public IEnumerator DelayInstructions()
+    {
+        yield return new WaitForSeconds(TimeToWaitForInstructions);
+        Instructions.SetActive(true);
+        _canExit = true;
+    }
+}
