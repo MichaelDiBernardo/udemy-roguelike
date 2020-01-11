8df4d3e1a07034a373dd763bf28fc3a6d0cb7963
MichaelDiBernardo <mikedebo@gmail.com>
2020-01-11 15:58:40 -0500

    After lecture 58.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string StartLevel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(StartLevel);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
