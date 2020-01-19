9c3f6cc34c39b353b22dfdece6b931049a6c3682
MichaelDiBernardo <mikedebo@gmail.com>
2020-01-18 14:41:10 -1000

    End of lecture 64.
    - Includes changes for lecture 63, oops.

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject layoutRoom;
+    public int distanceToEnd;
+    public Color startRoomColor, endRoomColor;
+
+    public Transform currentCenter;
+
+    private void Start()
+    {
+        GameObject firstRoom = Instantiate(layoutRoom, currentCenter.position, currentCenter.rotation);
+        firstRoom.GetComponent<SpriteRenderer>().color = startRoomColor;
+    }
+}
